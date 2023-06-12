17c8c37e302036fc7b107c404d3c2703c8ae7cca
vikram2000b <vikram2000b@gmail.com>
2023-06-12 05:35:00 -0700

    Add Quantile Loss

--- utils.py
+++ utils.py
@@ -5,6 +5,29 @@ import pandas as pd
 from torch.utils.data import DataLoader
 from torch.nn import functional as F
 
+class QuantileLoss(nn.Module):
+    ## From: https://medium.com/the-artificial-impostor/quantile-regression-part-2-6fdbc26b2629
+
+    def __init__(self, quantiles):
+        ##takes a list of quantiles
+        super().__init__()
+        self.quantiles = quantiles
+        
+    def forward(self, preds, target):
+        assert not target.requires_grad
+        assert preds.size(0) == target.size(0)
+        losses = []
+        for i, q in enumerate(self.quantiles):
+            errors = target - preds[:, i]
+            losses.append(
+                torch.max(
+                   (q-1) * errors, 
+                   q * errors
+            ).unsqueeze(1))
+        loss = torch.mean(
+            torch.sum(torch.cat(losses, dim=1), dim=1))
+        return loss
+
 def training_step(model, batch, device):
     images, labels = batch 
     images, labels = images.to(device), labels.to(device)
